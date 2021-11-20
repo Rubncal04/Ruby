42d808392bd15bf7f039f7d9d34d7d413c56215f
Ruben Gomez <rubncal.95@gmail.com>
2021-11-20 12:17:51 -0500

    it was added to folder called 'conditionals'

---
+++ conditionals/exercise_6_condition.rb
@@ -0,0 +1,9 @@
+=begin
+5. Una persona se encuentra con un problema de comprar un automóvil
+o un terreno, los cuales cuestan exactamente lo mismo. Sabe que
+mientras el automóvil se devalúa, con el terreno sucede lo contrario.
+Esta persona comprará el automóvil si al cabo de tres años la
+devaluación de este no es mayor que la mitad del incremento del
+valor del terreno. Ayúdale a esta pesona a determinar si debe o no
+comprar el automóvil
+=end
