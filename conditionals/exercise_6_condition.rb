971724f94d59e31cd1f96243ad89215e92366263
Ruben Gomez <rubncal.95@gmail.com>
2021-11-20 12:40:56 -0500

    it was modified the exercise 6 of conditionals/ruby

--- conditionals/exercise_6_condition.rb
+++ conditionals/exercise_6_condition.rb
@@ -1,9 +1,23 @@
 =begin
-5. Una persona se encuentra con un problema de comprar un automóvil
-o un terreno, los cuales cuestan exactamente lo mismo. Sabe que
-mientras el automóvil se devalúa, con el terreno sucede lo contrario.
-Esta persona comprará el automóvil si al cabo de tres años la
-devaluación de este no es mayor que la mitad del incremento del
-valor del terreno. Ayúdale a esta pesona a determinar si debe o no
-comprar el automóvil
+6. En una fábrica de computadoras se planea ofrecer a los clientes un
+descuento que dependerá del número de computadoras que
+compre. Si las computadoras son menos de cinco se les dará un 10%
+de descuento sobre el total de la compra; si el número de
+computadoras es mayor o igual a cinco pero menos de diez se le
+otorga un 20% de descuento; y si son 10 o más se les da un 40%. El
+precio de cada computadora es de $11.000.
 =end
+
+print "How many computers do you want to take? "
+num_pc = gets.chomp.to_i
+
+price_pc = 11000
+price_total_pc = 11000 * num_pc
+
+if num_pc >= 5 && num_pc < 10
+  puts "You should pay: $#{price_total_pc - (price_total_pc * 0.20)}"
+elsif num_pc >= 10
+  puts "You should pay: $#{price_total_pc - (price_total_pc * 0.40)}"
+else
+  puts "You should pay: $#{price_total_pc - (price_total_pc * 0.10)}"
+end
