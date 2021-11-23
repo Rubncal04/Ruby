c2866f94c6c79a6f91f2f35752021a7da87d427b
Ruben Gomez <rubncal.95@gmail.com>
2021-11-22 23:17:46 -0500

    modified exercise 1 of arrays

--- arrays/exercise_1_array.rb
+++ arrays/exercise_1_array.rb
@@ -9,8 +9,8 @@ d. El menor Elemento
 print "Tell me one number: "
 highest_num = gets.chomp.to_i
 
-array_all = %w[]
-array_all[0] = highest_num
+array_all = [highest_num]
+# array_all[0] = highest_num
 smallest_num = highest_num
 
 (1..6).each do |num|
