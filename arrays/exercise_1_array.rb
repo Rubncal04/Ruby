689f22d92a58b246a2ce69797c1e151994fac0c6
Ruben Gomez <rubncal.95@gmail.com>
2021-11-22 23:15:24 -0500

    new folder and new exercise of arrays/ruby

---
+++ arrays/exercise_1_array.rb
@@ -0,0 +1,32 @@
+=begin
+1. De los n elementos de un vector dado calcule:
+a. La sumatoria
+b. La productoria
+c. El Mayor Elemento
+d. El menor Elemento
+=end
+
+print "Tell me one number: "
+highest_num = gets.chomp.to_i
+
+array_all = %w[]
+array_all[0] = highest_num
+smallest_num = highest_num
+
+(1..6).each do |num|
+  print "Tell other number: "
+  number = gets.chomp.to_i
+  array_all << number
+
+  if array_all[num] < smallest_num
+    smallest_num = array_all[num]
+  end
+
+  if array_all[num] > highest_num
+    highest_num = array_all[num]
+  end
+end
+
+puts "This is the array: #{array_all}"
+puts "The highest number is: #{highest_num}"
+puts "The smallest number is: #{smallest_num}"
